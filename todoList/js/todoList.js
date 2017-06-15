17975e98ca8597dc0c29d6de8ff7a5d78b6d2ba6
uchang7194 <suyoung7194@gmail.com>
2017-06-15 21:18:01 +0900

    delete list 만듬 보완해야함

--- todoList/js/todoList.js
+++ todoList/js/todoList.js
@@ -59,33 +59,11 @@
 
     // 동적 생성
     var add_btn = document.querySelector('.add-btn');
+    var del_all_btn = document.querySelector('.del-all-btn');
     var input_text = document.querySelector('#todoInput');
+    var del_storage_list = document.querySelector('.del-list');
 
-    function setElements(input) {
-        var li = document.createElement('li');
-        var span = document.createElement('span');
-        var text = document.createTextNode(input.value);
-        var del_btn = document.createElement('button');
-        var close_marker = document.createElement('i');
-        
-        input.value = '';
-
-        close_marker.setAttribute('class', 'fa fa-times');
-        close_marker.setAttribute('aria-hidden', 'true');
-        del_btn.setAttribute('class', 'del-btn');
-        
-        del_btn.onclick = function() {
-            var parent_li = del_btn.parentNode;
-            todoList.removeChild(parent_li);
-        }
-
-        span.appendChild(text); 
-        del_btn.appendChild(close_marker);
-        li.appendChild(span);
-        li.appendChild(del_btn);
-        todoList.appendChild(li);
-    }
-
+    console.log(del_storage_list);
     input_text.onkeypress = function(e) {
         if(e.keyCode === 13) { 
             setElements(this);
@@ -101,4 +79,73 @@
         
     }
 
+    var del_el_arr = [];
+
+    del_all_btn.onclick = function() {
+        var list_length = todoList.querySelectorAll('li').length;    
+        removeAll(list_length, true);
+    }
+
+    function checkedString(text) {
+        var temp = text.value;
+        if(temp === '' || temp.trim().length === 0) { 
+            return false; 
+        } else {
+            return true;
+        }
+    }
+    function remove(node) {
+        var ul_class = node.parentNode.getAttribute('class');
+
+        if(ul_class === 'todoList') {
+            todoList.removeChild(node);
+        } else if(ul_class === 'del-list') {
+            del_storage_list.removeChild(node);
+        }
+    }
+    function removeAll(len, isStored) {
+        isStored = isStored || false;
+
+        for(var i = 0; i < len; i++) {
+            var li_el = document.querySelector('li');
+            del_el_arr.push(li_el);
+            todoList.removeChild(li_el);
+        }
+        console.log(del_el_arr, isStored);
+        if(isStored) {
+            for(var i = 0, l = del_el_arr.length; i < l; i++) {
+                console.log(del_el_arr[i]);
+                del_storage_list.appendChild(del_el_arr[i]);
+            }
+        }
+    }
+    var li_number = 0;
+
+    function setElements(input) {
+        var li = document.createElement('li');
+        var span = document.createElement('span');
+        var del_btn = document.createElement('button');
+        var close_marker = document.createElement('i');
+        
+        if(!checkedString(input)) { throw '내용이 없습니다.' }
+
+        var text = document.createTextNode(input.value);
+        input.value = '';
+
+        close_marker.setAttribute('class', 'fa fa-times');
+        close_marker.setAttribute('aria-hidden', 'true');
+        del_btn.setAttribute('class', 'del-btn');
+        li.setAttribute('role-index', li_number++);
+        
+        del_btn.onclick = function() {
+            var parent_li = del_btn.parentNode;
+            remove(parent_li);
+        }
+
+        span.appendChild(text); 
+        del_btn.appendChild(close_marker);
+        li.appendChild(span);
+        li.appendChild(del_btn);
+        todoList.appendChild(li);
+    }
 }(window));
